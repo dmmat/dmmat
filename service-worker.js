517e72f5c33a18ef6eca9e054e1a4fccba11ae65
Dmytro Matsiuk <dmmat@users.noreply.github.com>
2025-01-30 18:08:00 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 const CACHE_NAME = 'rf433-ble-cache-v1';
 const ASSETS_TO_CACHE = [
   './',
-  './index.html',
+  './rf433.html',
   './manifest.json',
   './icons/icon-192.png',
   './icons/icon-512.png'
